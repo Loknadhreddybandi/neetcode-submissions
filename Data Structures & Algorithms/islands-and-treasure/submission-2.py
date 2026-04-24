f28729a1fd2ce7bc4da8c08e1002c6c6a568dbd4
Loknadhreddybandi <155716599+Loknadhreddybandi@users.noreply.github.com>
2026-04-24 16:58:30 -0500

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows = len(grid)
+        cols = len(grid[0])
+        directions =[[0,1],[0,-1],[1,0],[-1,0]]
+        infinity = 2147483647
+        queue = collections.deque()
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==0:
+                    queue.append((r,c))
+        dist = 0
+        while queue:
+            for _ in range(len(queue)):
+                row,col = queue.popleft()
+                for dr,dc in directions:
+                    nr = row+dr
+                    nc = col + dc
+                    if (nr in range(rows) and (nc in range(cols)) and grid[nr][nc]==infinity):
+                        grid[nr][nc]= dist + 1
+                        queue.append((nr,nc))
+            dist +=1
+
+       
